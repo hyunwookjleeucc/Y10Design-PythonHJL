b1cfb828582069a645755efe2d47f3daf8d30a11
Justin Lee <justin.lee22@ucc.on.ca>
2019-10-06 21:58:14 -0400

    api update

--- exampleapi.py
+++ exampleapi.py
@@ -5,6 +5,8 @@ myapi.py
 '''
 
 import requests
+import json
+import pprint
 
 # Find APIs at - https://apilist.fun/
 # some will require an API key, boo hiss!
@@ -18,7 +20,7 @@ import requests
 # https://api.funtranslations.com/translate/chef.json?text=I%20like%20upper%20canada%20college
 
 def writeHTML(data):
-	myfile = open("myapi.html","w")
+	myfile = open("exampleapi.html","w")
 	myfile.write("<h1>JSON file returned by API call</h1>")
 	myfile.write("<p>Copy and paste to <a href='https://jsoneditoronline.org/'>JSON editor</a> for pretty format.</p>")
 	myfile.write(data)
@@ -26,22 +28,20 @@ def writeHTML(data):
 
 def main():
 	# use API to get place info
-	response = requests.get("https://geo-info.co/43.65,-79.40")
+	response = requests.get("https://financialmodelingprep.com/api/cryptocurrency?datatype=json")
 
 	# if API call is correct
 	if (response.status_code == 200):
-		data = response.content # response comes in as byte data type
-		data_as_str = data.decode()	# decode to str
-		writeHTML(data_as_str)  # call function to write string data to HTML file
-
-		# load as a JSON to access specific data more easily
+		data = response.content
 		datajson = response.json()
-		cities = datajson['nearbyCities']
-		for city in cities:
-			print(city)
-		
-	else:
-		data = "Error has occured"
-		writeHTML(data)
+		dataPoints = datajson['BTC']
+		pprint.pprint(datajson)
+
+		for point in dataPoints:
+			print(f"{point['ticker']}: \n\tPrice; {point['price']} \n\tChange; {point['changes']}\n")
+			
+		else:
+			data = "Error has occured"
+			writeHTML(data)
 
 main()
