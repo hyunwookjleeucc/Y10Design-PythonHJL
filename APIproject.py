fbbdd52556906e05811760c4c78aec1dcd06e4e0
Justin Lee <justin.lee22@ucc.on.ca>
2019-10-11 21:30:47 -0400

    graph attempt

--- APIproject.py
+++ APIproject.py
@@ -1,36 +1,37 @@
 #October 8, 2019
 #Hyunwook Justin Lee
-#October 8, 2019
+#Upper Canada College
 
 import requests
 import json
 import pprint
- 
+list1=[]
+list2=[]
 
-def writeHTML(data):
-	myfile = open("exampleapi.html","w")
-	myfile.write("<h1>CryptoCurrencies Information API </h1>")
-	myfile.write("<p>Copy and paste to <a href='https://jsonprettyprint.com'>JSON editor</a> for pretty format.</p>")
-	myfile.write(data)
-	myfile.close()
+def writeHTML(datajson):
+    print('l')
+    myfile = open("exampleapi.html","w")
+    myfile.write("<!DOCTYPE HTML><html><head></head><body><script src=" 'https://cdn.jsdelivr.net/npm/chart.js@2.8.0' "></script><script src=" 'https://canvasjs.com/assets/script/canvasjs.min.js' "></script><script>var ctx = document.getElementById('chartjs-0').getContext('2d');var chart = new Chart(ctx, {type: 'bar',data: {labels: [")
+    for i in range(len(datajson['cryptocurrenciesList'])):
+        if i !=0:
+            myfile.write("," + datajson['cryptocurrenciesList'][i]['ticker'])
+        else:
+            myfile.write(datajson['cryptocurrenciesList'][i]['ticker'])
+    myfile.write("],datasets: [{label: 'Stock Price',data: [")
+    for i in range(len(datajson['cryptocurrenciesList'])):
+        if i !=0:
+            myfile.write("," + datajson['cryptocurrenciesList'][i]['price'])
+        else:
+            myfile.write(str(datajson['cryptocurrenciesList'][i]['price']))
+    myfile.write("],pointRadius:0}]},options: {}});</script></body></html>")
+   
+    myfile.close()
 
 def main():
 	response = requests.get("https://financialmodelingprep.com/api/v3/cryptocurrencies")
 	
-	if (response.status_code == 200):
-
-		data = response.content
-		data_as_str = data.decode()
-		writeHTML(data_as_str)
-		datajson = response.json()
-		dataPoints = datajson['cryptocurrenciesList']
-		pprint.pprint(datajson)
-
-		for point in dataPoints:
-			print(f"{point['ticker']}: \n\tName; {point['name']} \n\tPrice; {point['price']} \n\tChange; {point['changes']}\n")
-			
-	else:
-		data = "Error has occured"
-		writeHTML(data)
+	if (response.status_code == 200):		
+        datajson = response.json()
+		writeHTML(str(datajson))
 
 main()
